1b6f1e4e528dcd8975cf252bf2c44ee54c5a01ee
Chanuth <80547770+Chanuth10@users.noreply.github.com>
2023-02-21 01:16:11 +0530

    Implment add my task

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -7,6 +7,20 @@ import '../widgets/tasks_list.dart';
 class TasksScreen extends StatelessWidget {
   TasksScreen({Key? key}) : super(key: key);
 
+  TextEditingController titleController = TextEditingController();
+
+  void _addTask(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (context) => SingleChildScrollView(
+              child: Container(
+                padding: EdgeInsets.only(
+                    bottom: MediaQuery.of(context).viewInsets.bottom),
+                child: AddTaskScreen(titleController: titleController),
+              ),
+            ));
+  }
+
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<TasksBloc, TasksState>(
@@ -36,12 +50,59 @@ class TasksScreen extends StatelessWidget {
             ],
           ),
           floatingActionButton: FloatingActionButton(
-            onPressed: () {},
+            onPressed: () => _addTask(context),
             tooltip: 'Add Task',
             child: const Icon(Icons.add),
           ),
         );
       },
+    );
+  }
+}
+
+class AddTaskScreen extends StatelessWidget {
+  const AddTaskScreen({
+    Key? key,
+    required this.titleController,
+  }) : super(key: key);
+
+  final TextEditingController titleController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(20),
+      child: Column(children: [
+        const Text(
+          'Add Task',
+          style: TextStyle(fontSize: 24),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        TextField(
+          autofocus: true,
+          controller: titleController,
+          decoration: const InputDecoration(
+            label: Text('Titla'),
+            border: OutlineInputBorder(),
+          ),
+        ),
+        TextButton(
+          onPressed: () => Navigator.pop(context),
+          child: const Text('cancel'),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            var task = Task(
+              title: titleController.text,
+            );
+            context.read<TasksBloc>().add(AddTask(task: task));
+            Navigator.pop(context);
+          },
+          child: const Text('Add'),
+        ),
+      ]),
     );
   }
 }
