4c6131eba85a74656a4103581b1f31a85a999f90
Chanuth <80547770+Chanuth10@users.noreply.github.com>
2023-02-21 01:31:42 +0530

    Design popup box for adding new task

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -3,12 +3,16 @@ import 'package:flutter_tasks_app/models/task.dart';
 
 import '../blocs/bloc_exports.dart';
 import '../widgets/tasks_list.dart';
+import 'add_task_screen.dart';
 
-class TasksScreen extends StatelessWidget {
-  TasksScreen({Key? key}) : super(key: key);
+class TasksScreen extends StatefulWidget {
+  const TasksScreen({Key? key}) : super(key: key);
 
-  TextEditingController titleController = TextEditingController();
+  @override
+  State<TasksScreen> createState() => _TasksScreenState();
+}
 
+class _TasksScreenState extends State<TasksScreen> {
   void _addTask(BuildContext context) {
     showModalBottomSheet(
         context: context,
@@ -16,7 +20,7 @@ class TasksScreen extends StatelessWidget {
               child: Container(
                 padding: EdgeInsets.only(
                     bottom: MediaQuery.of(context).viewInsets.bottom),
-                child: AddTaskScreen(titleController: titleController),
+                child: const AddTaskScreen(),
               ),
             ));
   }
@@ -31,7 +35,7 @@ class TasksScreen extends StatelessWidget {
             title: const Text('Tasks App'),
             actions: [
               IconButton(
-                onPressed: () {},
+                onPressed: () => _addTask(context),
                 icon: const Icon(Icons.add),
               )
             ],
@@ -56,53 +60,6 @@ class TasksScreen extends StatelessWidget {
           ),
         );
       },
-    );
-  }
-}
-
-class AddTaskScreen extends StatelessWidget {
-  const AddTaskScreen({
-    Key? key,
-    required this.titleController,
-  }) : super(key: key);
-
-  final TextEditingController titleController;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(20),
-      child: Column(children: [
-        const Text(
-          'Add Task',
-          style: TextStyle(fontSize: 24),
-        ),
-        const SizedBox(
-          height: 10,
-        ),
-        TextField(
-          autofocus: true,
-          controller: titleController,
-          decoration: const InputDecoration(
-            label: Text('Titla'),
-            border: OutlineInputBorder(),
-          ),
-        ),
-        TextButton(
-          onPressed: () => Navigator.pop(context),
-          child: const Text('cancel'),
-        ),
-        ElevatedButton(
-          onPressed: () {
-            var task = Task(
-              title: titleController.text,
-            );
-            context.read<TasksBloc>().add(AddTask(task: task));
-            Navigator.pop(context);
-          },
-          child: const Text('Add'),
-        ),
-      ]),
     );
   }
 }
